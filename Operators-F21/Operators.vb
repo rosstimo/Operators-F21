bdf92dde00d0dbfebb7e9f191a5985a2ec32f60f
rosstimo <rosstimo@isu.edu>
2021-08-30 08:39:24 -0600

    add division examples

--- Operators-F21/Operators.vb
+++ Operators-F21/Operators.vb
@@ -2,14 +2,18 @@
 
     Sub Main()
 
-        Console.WriteLine(2 + 2)
+        'Console.WriteLine(2 + 2)
 
-        Console.WriteLine(2 - 2)
+        'Console.WriteLine(2 - 2)
 
-        Console.WriteLine(2 * 2)
+        'Console.WriteLine(2 * 2)
 
-        Console.WriteLine(2 / 2)
+        'Console.WriteLine(2 / 2)
 
+
+        Console.WriteLine(7 / 3)
+        Console.WriteLine(7 \ 3)
+        Console.WriteLine(7 Mod 3)
 
         Console.Read()
 
