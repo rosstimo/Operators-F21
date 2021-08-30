0c28a750920a47b80318f88ed9fcff590923390b
rosstimo <rosstimo@isu.edu>
2021-08-30 08:56:05 -0600

    add compound operator examples

--- Operators-F21/Operators.vb
+++ Operators-F21/Operators.vb
@@ -11,9 +11,32 @@
         'Console.WriteLine(2 / 2)
 
 
-        Console.WriteLine(7 / 3)
-        Console.WriteLine(7 \ 3)
-        Console.WriteLine(7 Mod 3)
+        'Console.WriteLine(7 / 3)
+        'Console.WriteLine(7 \ 3)
+        'Console.WriteLine(7 Mod 3)
+
+        'Console.WriteLine("hello" & " " & "world")
+        'Dim x As String
+        'For i = 0 To 10
+        '    'x &= "*"
+        '    x = x & "*"
+        '    Console.WriteLine(x)
+        'Next
+
+        'Dim y As Integer
+        'For i = 0 To 10
+        '    y += 5
+        '    Console.WriteLine(y)
+        'Next
+
+
+        'Console.WriteLine(16 >= 6)
+
+        Console.Beep(392, 500) 'G4'
+        Console.Beep(440, 500) 'A4'
+        Console.Beep(349, 750) 'F4'
+        Console.Beep(196, 750) 'G3'
+        Console.Beep(262, 900) 'C4'
 
         Console.Read()
 
